2fc4240985b8bfaa4aa71dfbc23c9bfe954967db
Sonia Camacho <43547729+soniacamacho@users.noreply.github.com>
2020-03-13 11:31:56 -0700

    WE ARE DONE!!!:)

--- ours.py
+++ ours.py
@@ -6,6 +6,7 @@ import math
 import sys
 import time
 from timeit import default_timer
+
 class City  : 
     def __init__(self, id, x, y):
         self.id = id
@@ -13,8 +14,6 @@ class City  :
         self.y = y
         self.visited = False 
 
-    def __str__(self):
-        return f"City {self.id} {self.x} {self.y} {self.visited}"
 
 
 # *****************************************************************************
@@ -29,9 +28,12 @@ def calculateDistance(city1, city2):
 #******************************************************************************   
 def calculateTotalDistance(path):
     total = 0
-    for idx in range(0, len(path)-1):
-        total += calculateDistance(path[idx], path[idx+1])
-    total += calculateDistance(path[len(path)-1], path[0])   
+    last_stop = path[0]
+    for i, stop in enumerate(path):
+        if i != 0:
+            total += calculateDistance(last_stop, stop)
+        last_stop = stop
+    total += calculateDistance(last_stop, path[0])
     return total
 #*****************************************************************************
 # Function to get the name of the file and access read permissions and collect
@@ -49,14 +51,13 @@ def read_into_dict_list(filename):
             dict_entry['visited'] = False
             dict_from_file.append(dict_entry)
     return dict_from_file
-
 #******************************************************************************
 # Function to get the name of the file and re name and append the .tours 
 #******************************************************************************
 def output_file_rename(filename, tour, distance):
     #create the output file with the new name 
     output_file =  open (filename + ".tour", "w") 
-    print("\nOUTPUT FILE NAME: ", filename + ".tour") 
+    #print("\nOUTPUT FILE NAME: ", filename + ".tour") 
     #write the length of the file plus 1 as specified in the program description
     output_file.write(str(distance) + '\n')
     #properly print
@@ -88,47 +89,48 @@ def calc_closest_neighbor(source, targets):
 # return Final_result= Result
 #******************************************************************************
 def calc_neighbors(path):
-    build_path = [] #setting the empty path that will build the new path will all the cities 
-    current_city = path.pop(0) #get our current city that were at 
-    build_path.append(current_city) #all this city to our new path were building
-    while path != []: #while the path is not empty 
-        node = calc_closest_neighbor(current_city, path) #get the closest neighbor
-        current_city = node #set the new current city to be the node
-        path.remove(node) #pop that node off our OG path 
-        build_path.append(current_city) #and add it into the new path we built       
-    return build_path #return our new path we built
+    build_path = []  # setting the empty path that will build the new path will all the cities
+    current_city = path.pop(0)  # get our current city that were at
+    build_path.append(current_city)  # all this city to our new path were building
+    while path != []:  # while the path is not empty
+        node = calc_closest_neighbor(current_city, path)  # get the closest neighbor
+        current_city = node  # set the new current city to be the node
+        path.remove(node)  # pop that node off our OG path
+        build_path.append(current_city)  # and add it into the new path we built
+    return build_path  # return our new path we built
+
 #******************************************************************************
 # Function to perform the 2-opt stuff
 #******************************************************************************
-def two_opt(path):
+def two_opt_fast(path):
+    #print(path)
     flag = 1
     while flag:
         flag = 0
-        best_distance = calculateTotalDistance(path)
         for i in range(1, len(path) - 2):
             for j in range(i + 1, len(path)):
+                time_rem = 300 - (time.time() - start)
+                if time_rem < 2.0:
+                    return path
                 if j - i == 1:
-                    continue  # changes nothing, skip then
-                new_path = []
-                # 1. take path[0] to path[i-1] and add them in order to new_path
-                for index in range(0, i):
-                    new_path.append(path[index])
-
-                # 2. take path[i] to path[k] and add them in reverse order to new_path
-                for index in range(j, i-1, -1):
-                    new_path.append(path[index])
-
-                # 3. take path[k+1] to end and add them in order to new_path
-                for index in range(j+1, len(path)):
-                    new_path.append(path[index])
-                new_distance = calculateTotalDistance(new_path)
-                #new_path = path[:]    # Creates a copy of path
-                #new_path[i:j] = path[j - 1:i - 1:-1]  # this is the 2-optSwap since j >= i we use -1
-                if calculateTotalDistance(new_path) < calculateTotalDistance(path):
-                    path = new_path    # change current path to best
-                    best_distance = new_distance
-                    flag = 1     
+                    continue
+                #print("here")
+                new_path = path[:]
+                #print(new_path)
+                new_path[i:j] = path[j-1:i-1:-1]
+                #print(new_path)
+                if cost_change(new_path[i - 1], new_path[i], new_path[j - 1], new_path[j]) < 0:
+                    path = new_path
+                    flag = 1
     return path
+
+#******************************************************************************
+# Function to perform the cost difference 
+#******************************************************************************
+def cost_change(v1, v2, v3, v4):
+    #print("calcing...", v1, v2, v3, v4)
+    return (calculateDistance(v1, v2) + calculateDistance(v3, v4)) - (calculateDistance(v1, v3) + calculateDistance(v2, v4))
+
 #******************************************************************************
 #MAIN FUNCTION
 #******************************************************************************
@@ -138,7 +140,7 @@ if len(sys.argv) < 2:
     #get the file name from the user
     filename = input("Please enter the file name\n") 
     #print out the name of the file 
-    print("\nFILE NAME: ", filename)  
+    #print("\nFILE NAME: ", filename)  
 
 #otherwise we have the file name and start the program
 #style purpose
@@ -148,9 +150,26 @@ print("***********************************************************")
 #assign the proper comand line argument to the filename variable 
 filename = sys.argv[1]
 #print out the file name 
-print("\nINPUT FILE NAME: ", filename) 
-start = default_timer()
+#print("\nINPUT FILE NAME: ", filename) 
+#start = default_timer()
+#get the file data
+path = read_into_dict_list(filename)
 
+vecinos = calc_neighbors(path)
+#print("Finished vecinos")
+time_available = str(300 - (time.time() - start))
+#print("Time Available: ", time_available)
+two_opted = two_opt_fast(vecinos)
+calculated_total_d = calculateTotalDistance(two_opted)
+output_file_rename(filename, two_opted, calculateTotalDistance(two_opted))
+
+end = time.time()#default_timer()
+print("Runtime: " + str(end - start) + " seconds.")
+#optimal_length = 1573084
+#print("Accuracy: ", "(", calculated_total_d, ")/(", optimal_length, ")", (calculated_total_d * 1.0 / optimal_length * 1.0))
+
+
+'''
 #get the file data
 path = read_into_dict_list(filename)
 
@@ -165,3 +184,4 @@ path = two_opt(vecinos)
 output_file_rename(filename, path, calculateTotalDistance(path))
 end = default_timer()
 print("Runtime: " + str(end-start) + " seconds.")
+'''
